5677f93dadcf11245c9e12ffa50fa6563097bdd8
Domas Leščinskas <domas.lescinskas@gmail.com>
2026-05-15 22:35:36 +0300

    Added better player animation workflow Added extra graphics for upcoming characters

--- scripts/characters/component.gd
+++ scripts/characters/component.gd
@@ -1,10 +1,16 @@
 extends Node
 class_name CharacterComponent
 
-var player: CharacterBody2D
+var character: CharacterBody2D
+var graphics: AnimatedSprite2D
 
 func _ready() -> void:
-	player = get_parent() as CharacterBody2D
+	character = get_parent() as CharacterBody2D
+	graphics = character.get_node("AnimatedSprite2D") as AnimatedSprite2D
 
-	if not player:
+	if not character:
 		push_error("Component '" + name + "' must be a child of a CharacterBody2D!")
+
+	if not graphics:
+		push_warning("Component '" + name + "' requires an AnimatedSprite2D node for proper usage.")
+		graphics = null
--- scripts/characters/roman/movement.gd
+++ scripts/characters/roman/movement.gd
@@ -1,20 +1,64 @@
 extends CharacterComponent
 
-const SPEED = 100.0
-const JUMP_VELOCITY = 200.0
+const SPEED := 60.0
+const RUN_MULTIPLIER := 2.0
+const JUMP_VELOCITY := 300.0
+const ACCELERATION := 800.0
+const FRICTION := 600.0
 
 func _physics_process(delta: float) -> void:
-	if not player.is_on_floor():
-		player.velocity += player.get_gravity() * delta
+	apply_gravity(delta)
 
-	if Input.is_action_just_pressed("ui_accept") and player.is_on_floor():
-		player.velocity.y = -JUMP_VELOCITY
+	var direction := Input.get_axis("player_left", "player_right")
+	var isRunning := Input.is_action_pressed("player_run")
+	var isJumping := Input.is_action_just_pressed("player_jump")
 
-	var direction := Input.get_axis("ui_left", "ui_right")
+	move(delta, direction, isRunning)
+	jump(isJumping)
+
+	character.move_and_slide()
+
+func apply_gravity(delta: float) -> void:
+	if not character.is_on_floor():
+		character.velocity += character.get_gravity() * delta
+
+func jump(isJumping: bool) -> void:
+	if isJumping and character.is_on_floor():
+		character.velocity.y = -JUMP_VELOCITY
+
+func move(delta: float, direction: float, isRunning: bool):
+	var speed := SPEED * (RUN_MULTIPLIER if isRunning else 1.0)
 
 	if direction:
-		player.velocity.x = direction * SPEED
-	else:
-		player.velocity.x = move_toward(player.velocity.x, 0, SPEED)
+		character.velocity.x = move_toward(character.velocity.x, direction * speed, ACCELERATION * delta)
 
-	player.move_and_slide()
+	else:
+		if character.is_on_floor():
+			character.velocity.x = move_toward(character.velocity.x, 0, FRICTION * delta)
+		else:
+			character.velocity.x = move_toward(character.velocity.x, 0, (FRICTION * 0.1) * delta)
+
+	if character.is_on_floor():
+		var is_moving_physically = abs(character.velocity.x) > 0.1
+		
+		if graphics.animation == "jump_start" or graphics.animation == "jump_end":
+			if graphics.animation == "jump_start":
+				graphics.play("jump_end")
+			
+			if graphics.animation == "jump_end" and graphics.frame == graphics.sprite_frames.get_frame_count("jump_end") - 1:
+				graphics.animation = "idle"
+
+		elif not is_moving_physically:
+			graphics.animation = "idle"
+
+		else:
+			graphics.animation = "run" if isRunning else "walk"
+
+	else:
+		if character.velocity.y < 0:
+			graphics.animation = "jump_start"
+
+	if direction > 0:
+		graphics.flip_h = false
+	elif direction < 0:
+		graphics.flip_h = true
